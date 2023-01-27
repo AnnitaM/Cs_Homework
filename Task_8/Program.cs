dcb0a481d4e1875fc24d4319a8f1bfc0925b46e6
Annita Maniri <annitk@gmail.com>
2023-01-27 18:40:51 +0200

    add Task_8 все четные числа от 1 до N

---
+++ Task_8/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+Console.Clear(); 
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+int i = 1;
+if (number < 1)
+{
+    Console.Write("Enter a number grater than 1");
+    return;
+}
+while (i <= number)
+{
+    if (i % 2 == 0)
+    {
+      Console.Write($"{i} ");  
+    }
+    i ++; 
+}
