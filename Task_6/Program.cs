9d111d7b501772f465c28579f14fc099b1462a65
Annita Maniri <annitk@gmail.com>
2023-01-27 18:37:05 +0200

    add Task_6 проверка является ли число четным

---
+++ Task_6/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Clear();
+Console.Write("Enter a number: ");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0 & num % -2 == 0)
+{
+    Console.Write("It is even number");
+}
+else 
+{
+    Console.Write("It is odd number");
+}
+
